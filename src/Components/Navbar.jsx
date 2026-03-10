43f2f040d6868ae32021d63a6981f93044445afa
udommiracle2 <udommiracle2@gmail.com>
2026-03-10 21:28:39 +0100

    second commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <div>
+            <header>
+                <div className="logo">
+                    <h3>RealEstatePro</h3>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Navbar
